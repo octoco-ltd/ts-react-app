ea43315abc30cb7c279114aa6816495028313ba3
Calvin Drawbridge <calvin@octoco.ltd>
2022-10-13 10:44:57 +0200

    feat: added MUI and eslint

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# ts-react-app
-Octoco Typescript ReactJS App
+# Octoco ReactJS Template with TypeScript
+
+## Get Started
+1. run `yarn install`
+2. run `yarn start`
+3. Ensure your IDE is configured for ESLint using the `eslintrc.json` file
+
+## How it works
+
+## Features
+- ESLint and Prettier for code styling and formatting
---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Box, CssBaseline, Paper, Typography } from '@mui/material';
+
+function App() {
+  return (
+      <div>
+        <CssBaseline />
+        <Box
+            height="100vh"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+        >
+          <Paper
+              elevation={3}
+              sx={{ padding: "1rem", backgroundColor: "secondary.light" }}
+          >
+            <Typography color="primary.dark" variant="h1">
+              Starter App
+            </Typography>
+          </Paper>
+        </Box>
+      </div>
+  );
+}
+
+export default App;
