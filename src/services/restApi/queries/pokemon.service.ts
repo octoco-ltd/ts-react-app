0029909d7dfff052393de0c434b6cae7597fb76e
Christiaan <christiaan@Christiaans-MacBook-Pro.local>
2023-09-07 14:47:57 +0200

    na

--- src/services/restApi/queries/pokemon.service.ts
+++ src/services/restApi/queries/pokemon.service.ts
@@ -6,50 +6,44 @@ import { Endpoint, TagId } from './config/apiTags';
 
 
 const pokemonApi = baseApi.injectEndpoints({
-    endpoints: (builder) => ({
-        getAllUsers: builder.query<IPokemon, GridPaginationModel>({
-          query: (paginationModel) =>
-            `pokemon?limit=${paginationModel.pageSize}&offset=${paginationModel.page * paginationModel.pageSize}`,
-          providesTags: (result, error, page) =>
-            result
-              ? [
-                // Provides a tag for each pokemon in the current page,
-                // as well as the 'PARTIAL-LIST' tag.
-                ...result.results.map(({ name }: { name: string }) => ({ type: Endpoint.POKEMON as const, name })),
-                { type: Endpoint.POKEMON, id: TagId.PARTIAL_LIST },
-              ]
-              : [{ type: Endpoint.POKEMON, id: TagId.PARTIAL_LIST }],
-            extraOptions: { maxRetries: 1 } // will retry this 1 times
-        }),
-          // ############################## GET USER BY ID ###################################
-          getUserById: builder.query<{ isSuccess: boolean; value: any }, { userId: string }>({
-            query: ({ userId }: { userId: string }) => ({
-                url: `/pokemon/${userId}/`,
-                method: 'GET',
-            }),
-            providesTags: (result, error, arg) => [{ type: Endpoint.POKEMON, id: arg.userId }],
-            onQueryStarted: async (arg, { dispatch, queryFulfilled }) => {
-                try {
-                    //every time the user updates their own profile, we just update our userSlice db user as well with updated values
-                    //TODO: IMPLEMENT - AND IS THIS NECESSARY
-                    const { data } = await queryFulfilled;
-                    const userId: string | null = (store.getState() as RootState).user.user.dbUser
-                        .id;
-                    if (arg.userId === userId) {
-                        const user: any = (store.getState() as RootState).user.user;
-                    }
-                } catch (error) {
-                    console.error(error);
-                }
-            },
-          }),
-
-            // ############################# GET All LANGUAGES #############################
-            getLanguage: builder.query<any[], any>({
-              query: () => 'https://proteus-api.dev.voxcroft.com/api/1/Language',
-              extraOptions: { maxRetries: 0 } // will retry this 1 times
-          }),
+  endpoints: (builder) => ({
+    getAllUsers: builder.query<IPokemon, GridPaginationModel>({
+      query: (paginationModel) =>
+        `pokemon?limit=${paginationModel.pageSize}&offset=${paginationModel.page * paginationModel.pageSize}`,
+      providesTags: (result, error, page) =>
+        result
+          ? [
+            // Provides a tag for each pokemon in the current page,
+            // as well as the 'PARTIAL-LIST' tag.
+            ...result.results.map(({ name }: { name: string }) => ({ type: Endpoint.POKEMON as const, name })),
+            { type: Endpoint.POKEMON, id: TagId.PARTIAL_LIST },
+          ]
+          : [{ type: Endpoint.POKEMON, id: TagId.PARTIAL_LIST }],
+      extraOptions: { maxRetries: 1 } // will retry this 1 times
+    }),
+    // ############################## GET USER BY ID ###################################
+    getUserById: builder.query<{ isSuccess: boolean; value: any }, { userId: string }>({
+      query: ({ userId }: { userId: string }) => ({
+        url: `/pokemon/${userId}/`,
+        method: 'GET',
       }),
+      providesTags: (result, error, arg) => [{ type: Endpoint.POKEMON, id: arg.userId }],
+      onQueryStarted: async (arg, { dispatch, queryFulfilled }) => {
+        try {
+          //every time the user updates their own profile, we just update our userSlice db user as well with updated values
+          //TODO: IMPLEMENT - AND IS THIS NECESSARY
+          const { data } = await queryFulfilled;
+          const userId: string | null = (store.getState() as RootState).user.user.dbUser
+            .id;
+          if (arg.userId === userId) {
+            const user: any = (store.getState() as RootState).user.user;
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      },
+    }),
+  }),
 })
 
-export const { useGetAllUsersQuery, useLazyGetAllUsersQuery, useGetUserByIdQuery, useLazyGetUserByIdQuery, useGetLanguageQuery } = pokemonApi;
+export const { useGetAllUsersQuery, useLazyGetAllUsersQuery, useGetUserByIdQuery, useLazyGetUserByIdQuery } = pokemonApi;
