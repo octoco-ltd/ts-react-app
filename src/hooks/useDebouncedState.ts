ce0a3423f8135d13c62455290840f414eaef4495
Daniel Rossouw <daniel@rossouw.me>
2023-09-22 12:25:05 +0200

    useDebouncedState

---
+++ src/hooks/useDebouncedState.ts
@@ -0,0 +1,21 @@
+import { useCallback, useState } from 'react';
+import { debounce } from 'lodash';
+
+type DebouncedStateSetter<T> = (value: T) => void;
+
+export function useDebouncedState<T>(initialValue: T, delay = 500): [T, DebouncedStateSetter<T>] {
+  const [state, setState] = useState<T>(initialValue);
+
+  const debouncedSetState = useCallback(
+    debounce((value: T) => {
+      setState(value);
+    }, delay),
+    []
+  );
+
+  const handleStateChange: DebouncedStateSetter<T> = (value: T) => {
+    debouncedSetState(value);
+  };
+
+  return [state, handleStateChange];
+}
